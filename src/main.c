4b7155823cf1e782c0d28b7e509acf5e1bb77669
ViktoriaGatz <ledneva.margo@gmail.com>
2020-09-20 17:32:07 +0700

    Add author

--- src/main.c
+++ src/main.c
@@ -12,19 +12,22 @@ int main(int argc, char **argv) {
   }
 
   if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
-    printf("--help \t\t\t\t\t вывод меню помощи\n"
-           "--move ./file.txt ./bin/file.txt \t переместить файл "
-           "file.txt из текущей директории в папку bin\n"
-           "--copy ./file.txt ./bin/file.txt \t копировать файл "
-           "file.txt из текущей директории в папку bin\n"
-           "--delete ./file.txt \t\t\t удалить файл file.txt из текущей "
-           "директории\n"
-           "--scope ./file.txt \t\t\t узнать размер файла file.txt\n"
-           "--scope ./bin \t\t\t\t узнать размер директории bin\n"
-           "--ls ./bin \t\t\t\t отобразить название всех файлов в "
-           "директории bin\n"
-           "--procfs \t\t\t\t отобразить все процессы файловой системы "
-           "procfs\n");
+    printf(
+        "Авторы: Виктория Гацуля\n"
+        "Утилита proc предназначена для работы с файловой системой, а именно:\n"
+        "--help \t\t\t\t\t вывод меню помощи\n"
+        "--move ./file.txt ./bin/file.txt \t переместить файл "
+        "file.txt из текущей директории в папку bin\n"
+        "--copy ./file.txt ./bin/file.txt \t копировать файл "
+        "file.txt из текущей директории в папку bin\n"
+        "--delete ./file.txt \t\t\t удалить файл file.txt из текущей "
+        "директории\n"
+        "--scope ./file.txt \t\t\t узнать размер файла file.txt\n"
+        "--scope ./bin \t\t\t\t узнать размер директории bin\n"
+        "--ls ./bin \t\t\t\t отобразить название всех файлов в "
+        "директории bin\n"
+        "--procfs \t\t\t\t отобразить все процессы файловой системы "
+        "procfs\n");
   } else if (!strcmp(argv[1], "--move") || !strcmp(argv[1], "-m")) {
     if (argc < 4) {
       fprintf(stderr, "Недостаточно аргументов\n");
